aaa8439bf99a56f182a29373b85a6b0212fd9fa9
PerinbaSuresh <kaaviya062022@gmail.com>
2025-07-27 14:49:48 +0000

    lint fix

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -28,8 +28,6 @@ module.exports = {
           jsxSingleQuote: false,
           printWidth: 120,
           parser: 'babel-ts',
-          tabWidth: 4,
-          useTabs: false,
         },
       ],
       // Customize your rules here
